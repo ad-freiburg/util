2ba1c438350bfda569c68392b698800058a02e33
Patrick Brosi <brosi@informatik.uni-freiburg.de>
2025-12-03 15:06:24 +0100

    add functions for getting the minimum dimenions of the boundary or interior of geometries required for matching a DE-9IM filter

--- geo/DE9IMatrix.h
+++ geo/DE9IMatrix.h
@@ -42,6 +42,8 @@ const static uint16_t F = bit16(00);
 const static uint16_t D0 = bit16(01);
 const static uint16_t D1 = bit16(10);
 const static uint16_t D2 = bit16(11);
+const static uint16_t T = bit16(100);
+const static uint16_t STAR = bit16(101);
 
 class DE9IMatrix {
  public:
@@ -181,7 +183,7 @@ class DE9IMFilter {
     if (m[8] == '0') _eeVal = D0;
     if (m[8] == '1') _eeVal = D1;
     if (m[8] == '2') _eeVal = D2;
-    if (m[8] == 'T' || m[8] == 't') _eeVal = D2 + 1;
+    if (m[8] == 'T' || m[8] == 't') _eeVal = T;
   }
 
   bool matches(const DE9IMatrix a) const {
@@ -205,32 +207,83 @@ class DE9IMFilter {
     return true;
   }
 
+  uint16_t valAt(uint16_t i) const {
+    if (i == 8) return _eeVal;
+
+    uint16_t ret = STAR;
+
+    uint8_t eqMask = (_equalityMask & (3 << (i * 2))) >> (i * 2);
+    if (eqMask) {
+      uint8_t v = (_equalityValues & (3 << (i * 2))) >> (i * 2);
+      if (v <= D2) ret = v;
+    }
+
+    uint8_t trueMask = (_trueMask & (3 << (i * 2))) >> (i * 2);
+    if (trueMask) ret = T;
+
+    return ret;
+  }
+
+  int16_t minInteriorDim() const {
+    return std::min(minLeftInteriorDim(), minRightInteriorDim());
+  }
+
+  int16_t minLeftInteriorDim() const {
+    uint16_t ret = 0;
+    if (valAt(0) <= D2) ret = std::max(ret, valAt(0));
+    if (valAt(1) <= D2) ret = std::max(ret, valAt(1));
+    if (valAt(2) <= D2) ret = std::max(ret, valAt(2));
+
+    return static_cast<int16_t>(ret) - 1;
+  }
+
+  int16_t minRightInteriorDim() const {
+    uint16_t ret = 0;
+    if (valAt(0) <= D2) ret = std::max(ret, valAt(0));
+    if (valAt(4) <= D2) ret = std::max(ret, valAt(4));
+    if (valAt(7) <= D2) ret = std::max(ret, valAt(7));
+
+    return static_cast<int16_t>(ret) - 1;
+  }
+
+  int16_t minBoundaryDim() const {
+    return std::min(minLeftBoundaryDim(), minRightBoundaryDim());
+  }
+
+  int16_t minLeftBoundaryDim() const {
+    uint16_t ret = 0;
+    if (valAt(3) <= D2) ret = std::max(ret, valAt(3));
+    if (valAt(4) <= D2) ret = std::max(ret, valAt(4));
+    if (valAt(5) <= D2) ret = std::max(ret, valAt(5));
+
+    return static_cast<int16_t>(ret) - 1;
+  }
+
+  int16_t minRightBoundaryDim() const {
+    uint16_t ret = 0;
+    if (valAt(1) <= D2) ret = std::max(ret, valAt(1));
+    if (valAt(4) <= D2) ret = std::max(ret, valAt(4));
+    if (valAt(7) <= D2) ret = std::max(ret, valAt(7));
+
+    return static_cast<int16_t>(ret) - 1;
+  }
+
+  int16_t maxExteriorDim() const {
+    uint16_t ret = 3;
+    if (valAt(8) <= D2) ret = std::min(ret, valAt(8));
+    return static_cast<int16_t>(ret) - 1;
+  }
+
   std::string toString() const {
     std::string ret = "*********";
 
-    for (size_t i = 0; i < 8; i++) {
-      uint8_t eqMask = (_equalityMask & (3 << (i * 2))) >> (i * 2);
-      if (!eqMask) continue;
-      uint8_t v = (_equalityValues & (3 << (i * 2))) >> (i * 2);
-
-      if (v == F) ret[i] = 'F';
-      if (v == D0) ret[i] = '0';
-      if (v == D1) ret[i] = '1';
-      if (v == D2) ret[i] = '2';
+    for (size_t i = 0; i < 9; i++) {
+      if (valAt(i) == F) ret[i] = 'F';
+      if (valAt(i) == D0) ret[i] = '0';
+      if (valAt(i) == D1) ret[i] = '1';
+      if (valAt(i) == D2) ret[i] = '2';
+      if (valAt(i) == T) ret[i] = 'T';
     }
-
-    for (size_t i = 0; i < 8; i++) {
-      uint8_t trueMask = (_trueMask & (3 << (i * 2))) >> (i * 2);
-      if (!trueMask) continue;
-      ret[i] = 'T';
-    }
-
-    if (_eeVal == F) ret[8] = 'F';
-    if (_eeVal == D0) ret[8] = '0';
-    if (_eeVal == D1) ret[8] = '1';
-    if (_eeVal == D2) ret[8] = '2';
-    if (_eeVal == D2 + 1) ret[8] = 'T';
-    if (_eeVal == D2 + 2) ret[8] = '*';
 
     return ret;
   }
@@ -241,11 +294,15 @@ class DE9IMFilter {
            _equalityValues == other._equalityValues && _eeVal == other._eeVal;
   }
 
+  bool operator!=(const DE9IMFilter& other) const {
+    return !(operator==(other));
+  }
+
  private:
   uint16_t _equalityMask = 0;
   uint16_t _trueMask = 0;
   uint16_t _equalityValues = 0;
-  uint8_t _eeVal = D2 + 2;
+  uint8_t _eeVal = STAR;
 };
 
 // often used filters
